d8d03d95d23dea621e346bc359f6488332fc6387
Alibrown Julius <brownjulius980@gmail.com>
2026-04-29 23:22:15 +0100

    Initialize Flutter project for delivery live tracking

---
+++ lib/features/delivery/presentation/view/delivery_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DeliveryScreen extends StatelessWidget {
+  const DeliveryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:delivery_live_tracking/features/delivery/presentation/view/delivery_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Delivery Live Tracking',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const DeliveryScreen()
+    );
+  }
+}
